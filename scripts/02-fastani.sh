3a6f95a2fd42246473c8c0a9a09f0b935869083b
Alejandro De Santiago <adesa002@ucr.edu>
2025-04-30 23:01:31 -0400

    Update and rename 02-fastani.sh to scripts/02-fastani.sh

--- scripts/02-fastani.sh
+++ scripts/02-fastani.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=15G
 #SBATCH --time=7-00:00:00
-#SBATCH --mail-user=ad14556@gmail.com
+#SBATCH --mail-user=ad14556@uga.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH -e fastANI.err-%N
 #SBATCH -o fastANI.out-%N
@@ -18,6 +18,5 @@ module load dRep
 OUTPUT=/home/ad14556/pangenome-large/results/02-drep
 INPUT=/home/ad14556/pangenome-large/data/genomes
 
-#dRep dereplicate "$OUTPUT" -g "$INPUT"/* --S_algorithm fastANI --S_ani 0.999 --SkipMash  
 dRep compare "$OUTPUT" -g "$INPUT"/* --S_algorithm fastANI --S_ani 0.95 --SkipMash
 
